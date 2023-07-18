c366b713df089e12a5c5d4c306e77c22bb0b5458
TanWeibo <543622842@qq.com>
2023-07-18 14:59:33 +0800

    Add a exefile

--- 整人类/永远也猜不对的游戏/永远猜不对的游戏.py
+++ 整人类/永远也猜不对的游戏/永远猜不对的游戏.py
@@ -1,3 +1,3 @@
-while True:
+﻿while True:
     input('猜猜我在干什么：')
     print('猜错咯！')
--- 整人类/疯狂剪切板/疯狂剪切板.py
+++ 整人类/疯狂剪切板/疯狂剪切板.py
@@ -1,5 +1,8 @@
 from pyperclip import copy, paste
+import tkinter.messagebox as box
+import os
 
+box.showinfo('INFO', '按下确定即开始运行本程序。')
 copy('剪切板已经罢工了！！！')
 while True:
     try:
@@ -8,4 +11,5 @@ while True:
             copy('剪切板已经罢工了！！！')
     except KeyboardInterrupt:
         print('Thanks!')
+        os.system('pause')
         break
